cebfac095dc2694681425de58415fa422278d04a
prnjalghlt <60695582+prnjalghlt@users.noreply.github.com>
2020-03-09 00:55:43 +0530

    Create SUM.cpp

---
+++ SUM.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, Sum;
+	
+	cout << "Enter the value of A and B "<<endl;
+	cin >> a >> b;
+	Sum = a + b;
+	cout << "SUM="<< Sum << endl;
+}
