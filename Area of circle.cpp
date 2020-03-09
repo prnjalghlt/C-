a3bbedcada1a5e6d67ecbb7736981126af2f79c0
prnjalghlt <60695582+prnjalghlt@users.noreply.github.com>
2020-03-09 21:04:04 +0530

    Create Area of circle.cpp

---
+++ Area of circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float r, area;
+	cout << "Enter Radius" << endl;
+	cin >> r;
+	area = 3.14 * r * r;
+	cout << "Area of circle =" << area;
+
+}
